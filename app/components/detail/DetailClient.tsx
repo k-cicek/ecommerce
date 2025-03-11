8c66a0c509ba4f6c097163e460cdd234664528fd
k-cicek <kevserrcicek@gmail.com>
2025-03-11 23:10:22 +0300

    added card detail

--- app/components/detail/DetailClient.tsx
+++ app/components/detail/DetailClient.tsx
@@ -1,6 +1,9 @@
 "use client"
 
+import Image from "next/image";
 import PageContainer from "../containers/PageContainer";
+import Counter from "../general/Counter";
+import { useState } from "react";
 
 type Product = {
     id: string;
@@ -21,10 +24,34 @@ type Product = {
 };
 
 const DetailClient = ({ product }: { product: Product }) => {
+    const [cartProduct, setCartProduct] = useState({
+        productID: product.id,
+        quantity: 0,
+        price: product.price,
+    });
     return (
         <div className="my-10">
             <PageContainer>
-                {product?.name}
+                <div className="block md:flex gap-10 justify-center">
+                    <div className="relative h-[400px] w-[200px]">
+                        <Image src={product.image} fill alt="" />
+                    </div>
+                    <div className="w-1/2 space-y-3">
+                        <div className="text-xl md:text-2xl">
+                            {product.name}
+                        </div>
+                        <div className="text-slate-500">
+                            {product.description}
+                        </div>
+                        <div className="flex items-center gap-2">
+                            <div>STOK DURUMU:</div>
+                            {
+                                product.inStock ? <div className="text-green-500">Ürün Stokta Mevcut</div> : <div className="text-red-500">Ürün Stokta Mevcut Değil</div>
+                            }
+                        </div>
+                        <Counter cartProduct={cartProduct} increaseFunc={() => { }} decreaseFunc={() => { }} />
+                    </div>
+                </div>
             </PageContainer>
         </div>
     )
