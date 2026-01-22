a19fa152a9a0a1ce734f88628472609e69b39720
k-cicek <kcicek1@ogr.iu.edu.tr>
2026-01-22 16:16:05 +0300

    create admin panel

---
+++ app/components/admin/AdminSidebar.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import React from 'react'
+import AdminSidebarItem from './AdminSidebarItem'
+import { MdBorderOuter, MdDashboard, MdOutlineCreate } from 'react-icons/md'
+import { usePathname } from 'next/navigation'
+
+const AdminSidebar = () => {
+    const pathname = usePathname();
+    const adminPanel = [
+        {
+            name: "Özetler",
+            icon: MdDashboard,
+            url: '/admin'
+        },
+        {
+            name: "Ürün Olustur",
+            icon: MdOutlineCreate,
+            url: "/admin/create"
+        },
+        {
+            name: "Siparişlerim",
+            icon: MdBorderOuter,
+            url: "/admin/order"
+        }
+    ]
+    return (
+        <div className='w-1/6 border-r h-screen p-3 bg-orange-600'>
+            <div className='space-y-4'>
+                {
+                    adminPanel.map((admin, i) => (
+                        <AdminSidebarItem key={i} selected={pathname === admin.url} name={admin.name} icon={admin.icon} url={admin.url} />
+                    ))
+                }
+            </div>
+        </div>
+    )
+}
+
+export default AdminSidebar
---
+++ app/components/admin/AdminSidebarItem.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import { IconType } from "react-icons";
+
+interface AdminSidebarItemProps {
+    selected?: boolean;
+    name: string;
+    icon: IconType;
+    url: string;
+}
+
+const AdminSidebarItem: React.FC<AdminSidebarItemProps> = ({ selected, name, icon: Icon, url }) => {
+    return (
+        <div>
+            <Link className={`cursor-pointer flex items-center gap-2 ${selected ? "text-slate-600 font-bold" : "text-white font-medium"}`} href={url}>
+                <Icon size={25} />
+                <div>{name}</div>
+            </Link>
+        </div>
+    )
+}
+
+export default AdminSidebarItem
